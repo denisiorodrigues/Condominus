27ba575c1d4ce178cab773fe46e7e2fd823b715f
Denisio Rodrigues (Samasung) <denisio@ymail.com>
2018-12-24 19:06:11 -0300

    Alterando o Save Change para atualizar a data de cadastro sem precisar setar na mõ quando for adicionar e  não precisar fazer nada quando for edição, mesmo se setar, a informação não vai!

--- src/DR.Condominus.Infra.Data/Context/CondominusContext.cs
+++ src/DR.Condominus.Infra.Data/Context/CondominusContext.cs
@@ -36,5 +36,20 @@ namespace DR.Condominus.Infra.Data.Context
 
             base.OnModelCreating(modelBuilder);
         }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries()
+                .Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                    entry.Property("DataCadastro").IsModified = false;
+            }
+
+            return base.SaveChanges();
+        }
     }
 }
